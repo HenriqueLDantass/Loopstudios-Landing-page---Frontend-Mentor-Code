df829f7df9503883c86b12f41fc97c09aa47b8aa
perkles <henrique543@gmail.com>
2022-10-21 23:01:00 -0300

    Finalização do projeto

--- main/script.js
+++ main/script.js
@@ -1,16 +1,39 @@
 const Btn = document.getElementById('menu-hambuger');
 const image = document.getElementById('image');
-function toggleHamburger(event) {
-    if (event.type === 'touchstart') event.preventDefault();
+function toggleHamburger(e) {
+    if (e.type === 'touchstart') e.preventDefault();
     const nav = document.getElementById('nav');
     nav.classList.toggle('active');
     const active = nav.classList.contains('active');
-    event.currentTarget.setAttribute('aria-expanded', active);
+    e.currentTarget.setAttribute('aria-expanded', active);
     if (active) {
-        event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+        e.currentTarget.setAttribute('aria-label', 'Fechar Menu');
     } else {
-        event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+        e.currentTarget.setAttribute('aria-label', 'Abrir Menu');
     }
 }
 Btn.addEventListener('click', toggleHamburger)
 Btn.addEventListener('touchstart', toggleHamburger);
+
+
+
+const target = document.querySelectorAll('[data-anime]');
+const AnimationClass = 'animate';
+
+function AnimeScroll() {
+    const windowTop = window.pageYOffset + ((window.innerHeight * 5) / 6);
+    target.forEach(function (element) {
+        if ((windowTop) > element.offsetTop) {
+            element.classList.add(AnimationClass)
+        } else {
+            element.classList.remove(AnimationClass)
+        }
+    })
+}
+AnimeScroll();
+
+if (target.length) {
+    window.addEventListener('scroll', function () {
+        AnimeScroll();
+    })
+}
